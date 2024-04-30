dbfe273fa36d6e237bddf8e15fdea3d9ded787bd
Bruno Lima <brunoadl88@gmail.com>
2024-04-30 22:28:11 +0100

    Listing tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -80,7 +80,7 @@ class NotesController {
 
     const userTags = await knex("tags").where({ user_id });
 
-    const notesWithTags = userTags.map((note) => {
+    const notesWithTags = notes.map((note) => {
       const noteTags = userTags.filter((tag) => tag.note_id === note.id);
 
       return {
